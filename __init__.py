f5eb21c339651474a6263b4480193269305d4d67
Adil-ksl <kassouladil@gmail.com>
2024-11-20 16:41:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,11 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm
-
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -26,10 +24,8 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-  
-if __name__ == "__main__":
+  if __name__ == "__main__":
   app.run(debug=True)
